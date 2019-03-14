670942a53216a9dbd111dd0437da276b5afe6e55
Kim Burgess <kim@acaprojects.com>
2019-03-15 09:42:26 +1000

    add support for crystal play

--- docker-crystal.sh
+++ docker-crystal.sh
@@ -3,20 +3,32 @@
 set -o errexit
 set -o nounset
 
+# Get the version of Crystal to be using
 if [ -f shard.yml ]; then
-  # Naively extract the crystal version from shard.yml
+  # Naively extract from shard.yml
   CRYSTAL_VERSION=`grep 'crystal:' shard.yml | awk '{print $2}'`
 else
   (>&2 echo "Could not find shard.yml")
   CRYSTAL_VERSION="latest"
 fi
-
 (>&2 echo "⬡ Using crystal:${CRYSTAL_VERSION}")
+
+# Build any additional args needed for sub-commands
+declare -a DOCKER_ARGS=("")
+declare -a CRYSTAL_ARGS=("")
+case "$1" in
+  play)
+    PORT=`echo "$*" | grep -oP "(-p)|(\--port)\s+\K\d+" || echo 8080`
+    DOCKER_ARGS+="--publish ${PORT}:${PORT}"
+    CRYSTAL_ARGS+="--binding 0.0.0.0"
+    ;;
+esac
 
 docker run \
     --rm \
     --tty \
     --volume `pwd`:/data \
     --workdir /data \
+    ${DOCKER_ARGS} \
     crystallang/crystal:${CRYSTAL_VERSION} \
-    crystal $*
+    crystal $* ${CRYSTAL_ARGS}
